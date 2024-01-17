3bff3a589b74f189b474edc296e8ef30e8510a41
laurammoraes2@gmail.com <laura@poontoagencia.com>
2024-01-17 18:05:37 -0300

    init make test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css';
 
 function App() {
-  return ();
+  return (
+    <div>AC</div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App Component', () => {
+  test('should saw a calculator layout', () => {
+    const { getByLabelText } = render(<App />);
+    expect(getByLabelText('calculatorLayout')).toHaveStyle({
+      
+    })
+  })
+})
+ 
